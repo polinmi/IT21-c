168ffdfd7526613ecdb2954813307e1fc1cd18c6
polinmi <jeonmay7@gmail.com>
2024-10-04 22:03:54 +0800

    Added objects and properties and its values

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
-console.log("Hello Self");
-console.log("Lahamss ka ng all");
+//Input
+const person1 ={
+    name:"Pauline",
+    age:99,
+    occupation:"studyante pako uy"
+}
+//Process and Output
+console.log("Name: "+person1.name);
+console.log("Age: "+person1.age);
+console.log("Occupation: "+person1.occupation);
