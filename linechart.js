cb3dc836e78e781c49e9cbe6e405ed6e6403a88d
polinmi <jeonmay7@gmail.com>
2024-10-15 22:39:18 +0800

    Added a methods ChartCreator and asyncinit

---
+++ linechart.js
@@ -0,0 +1,14 @@
+class ChartCreator {
+    constructor(dataUrl) {
+        this.dataUrl = dataUrl;
+        this.chartData = null;
+    }
+
+    async init() {
+        await this.fetchData();
+        if (this.chartData) {
+            this.createCharts();
+        }
+    }
+
+}
